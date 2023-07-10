48dc24ecefd5764744df322dad2c1b8359e66604
yana93ser <90180341+yana93ser@users.noreply.github.com>
2023-07-10 15:42:32 +0300

    корректировки

--- tests/test_notlog.py
+++ tests/test_notlog.py
@@ -1,7 +1,7 @@
 import allure
 
 from pages.not_login import NotLogin
-from pages.users import test_user
+from pages.users import test_users
 
 
 # @allure.step('Покупка виниловой пластинки без регистрации на сайте')
@@ -16,8 +16,5 @@ def test_not_login():
         not_login.lp_on_page()
     with allure.step("Покупка пластинки незарегистрированным пользователем"):
         not_login.buy_not_logo()
-
-
     with allure.step("Подтверждение покупки"):
-        not_login.buy_name(test_user)
-
+        not_login.buy_name(test_users)
