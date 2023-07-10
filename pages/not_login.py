bf6c05782c9baaa783526b5d0b206470030d4a09
yana93ser <90180341+yana93ser@users.noreply.github.com>
2023-07-10 09:50:42 +0300

    диплом

---
+++ pages/not_login.py
@@ -0,0 +1,29 @@
+from selene import browser
+
+from pages.users import Users
+
+
+class NotLogin:
+    def open(self):
+        browser.open('https://imagine-club.com/')
+        return self
+
+    def finde_lp(self, name):
+        browser.element("#edit-search-api-views-fulltext").type(name).press_enter()
+        return self
+
+    def lp_on_page(self):
+        browser.element('#commerce-cart-add-to-cart-form-86089').click()
+        return self
+
+    def buy_not_logo(self):
+        browser.element("#block-commerce-popup-cart-commerce-popup-cart").click()
+        browser.element('#edit-checkout').click()
+        return self
+
+    def buy_name(self, user: Users):
+        browser.element('#edit-account-login-mail').type(user.email)
+        browser.element("#edit-customer-profile-billing-field-phone-und-0-value").type(user.phone)
+        browser.element('#edit-continue').press_enter()
+        browser.element('#edit-continue').press_enter()
+        return self
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import allure
+
+from pages.not_login import NotLogin
+from pages.users import test_user
+
+
+@allure.step("Покупка виниловой пластинки без регистрации на сайте")
+def test_not_login():
+    not_login = NotLogin()
+
+    with allure.step("Открыть страницу магазина"):
+        not_login.open()
+    with allure.step("Ввести в поле поиска исполнителя"):
+        not_login.finde_lp("sinatra frank")
+    with allure.step("Поиск необходимой пластинки на сайте"):
+        not_login.lp_on_page()
+    with allure.step("Покупка пластинки незарегистрированным пользователем"):
+        not_login.buy_not_logo()
+    with allure.step("Подтверждение покупки"):
+        not_login.buy_name(test_user)
